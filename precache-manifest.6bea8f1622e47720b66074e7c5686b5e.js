2b4ee176e46583f369e80c4bc2b10f4b2fcf46fd
Aek <ekkawin.v@gmail.com>
2020-11-20 20:05:18 +0700

    Updates

---
+++ precache-manifest.6bea8f1622e47720b66074e7c5686b5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5093abfdd095522775a32512b91a271",
+    "url": "/YWC-Website/index.html"
+  },
+  {
+    "revision": "a1f268dc442377e79bc0",
+    "url": "/YWC-Website/static/css/2.5cfd0e0c.chunk.css"
+  },
+  {
+    "revision": "201954998935c677700b",
+    "url": "/YWC-Website/static/css/main.7cc85c4f.chunk.css"
+  },
+  {
+    "revision": "a1f268dc442377e79bc0",
+    "url": "/YWC-Website/static/js/2.224ba4bf.chunk.js"
+  },
+  {
+    "revision": "4123a3d013fbd80e23797cc29f3bb57e",
+    "url": "/YWC-Website/static/js/2.224ba4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59f6a9a1311f7aa161a",
+    "url": "/YWC-Website/static/js/3.29ee27e2.chunk.js"
+  },
+  {
+    "revision": "201954998935c677700b",
+    "url": "/YWC-Website/static/js/main.122c7720.chunk.js"
+  },
+  {
+    "revision": "a18b1cd86c91d4ddbeab",
+    "url": "/YWC-Website/static/js/runtime-main.2747bed1.js"
+  },
+  {
+    "revision": "a5ce67f55f717fd5ddb6d5ccee4ba690",
+    "url": "/YWC-Website/static/media/dog.a5ce67f5.svg"
+  }
+]);
